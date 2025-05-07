bc99861971fb5688b44a74f90d342d483ae3c6c9
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-07 18:53:14 +0700

    tambah fungsi input dengan validasi maksimal 10 elemen

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -4,3 +4,21 @@ using namespace std; //Library
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+    
+}
