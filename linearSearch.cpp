6a9792c06d946a08e80a61f318ddfa1194b68a66
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-07 19:05:36 +0700

    implementasi algoritma Bubble Sort untuk array

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -31,3 +31,21 @@ void input()
     }
     
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element [j] = element [j + 1];
+                element [j + 1] = temp;
+            }
+        }
+    }
+    
+}
