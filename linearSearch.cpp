56d7b28c199ea1b8cbfb523451874f98d9912175
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-07 18:55:51 +0700

    cout for int nPanjang

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -20,5 +20,11 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
         }
     }
+
+    cout << "\n=========================\n";
+    cout << "       Masukkan Elemen Array   \n";
+    cout << "============================\n";
+    for (int i = 0; i < nPanjang; i++)
+    
     
 }
