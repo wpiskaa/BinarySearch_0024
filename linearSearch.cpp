79b713ca4afe1ce3d112219e52a50777f6319e3f
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-07 19:13:32 +0700

    tampilkan elemen array setelah diurutkan

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -50,3 +50,19 @@ void bubbleSortArray()
     }while (pass <= nPanjang - 1);
     
 }
+
+void display()
+{
+    cout << "\n================================================\n";
+    cout << "       Elemen Array setelah diurutkan (Asc)    \n";
+    cout << "==================================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element [j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+}
